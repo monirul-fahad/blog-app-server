a4d6a4d9bb1293da5b8cb3e76786f07f004dd9a2
monirul-fahad <immonirulfahad@gmail.com>
2022-01-01 15:39:54 +0600

    auth crated

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -14,6 +16,8 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.use("/", (req, res) => {
   console.log("hey this is main URL");
