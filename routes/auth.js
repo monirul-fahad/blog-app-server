20716e5e33015c1c5beb16119877ced3dbcaa5e6
Nazmus Sakib <nazmussakibppp22@gmail.com>
2022-01-02 00:07:37 +0600

    authentication details

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-
-const abc = {
-  username: "sakib",
-  email: "sakib@gmail.com",
-  password: "123456"
-}
-//Register
+//REGISTER
 router.post("/register", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPass,
     });
 
     const user = await newUser.save();
@@ -23,23 +20,20 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials!");
 
-// router.post("/register", async (req, res) => {
-//   try {
-//     const newUser = new User({
-//       username: abc.username,
-//       email: abc.email,
-//       password: abc.password,
-//     });
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong credentials!");
 
-//     const user = await newUser.save();
-//     console.log(user);
-//     res.json(user);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
-//Login
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
